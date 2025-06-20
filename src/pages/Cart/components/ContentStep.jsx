46ed637ed42dfa4526334361d75de289c8d3c53f
gamhong00 <honggambdbt@gmail.com>
2025-06-20 22:17:04 +0700

    code checkout2

--- src/pages/Cart/components/ContentStep.jsx
+++ src/pages/Cart/components/ContentStep.jsx
@@ -1,18 +1,19 @@
 import Contents from '@pages/Cart/components/contents/Contents';
 import { StepperContext } from '@contexts/StepperProvider';
 import { useContext } from 'react';
+import Checkout from '@pages/Cart/components/Checkout/Checkout';
 
 function ContentStep() {
     const { currentStep } = useContext(StepperContext);
 
-    console.log(currentStep);
+    // console.log(currentStep);
 
     const handleRenderContent = () => {
         switch (currentStep) {
             case 1:
                 return <Contents />;
             case 2:
-                return <div>Step 2 Content</div>;
+                return <Checkout />;
             case 3:
                 return <div>Step 3 Content</div>;
         }
